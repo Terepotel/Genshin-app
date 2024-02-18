01deca42520a50a2a2040a199f531651f1b8c405
terepotel <terepotel13@gmail.com>
2024-02-18 20:07:52 -0300

    agregar modal a charactersCard y se agrega lista de nombres con fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
 import CharacterCard from "./components/CharacterCard";
 import charactersData from "../public/charactersData.json";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [dataCharacters, setDataCharacters] = useState([])
+  useEffect(() => {
+    const fetchCharacterList= async() => {
+      const response=await fetch('https://genshin.jmp.blue/characters/')
+      const data=await response.json()
+    setDataCharacters(data)
+    }
+    fetchCharacterList()
+  }, [])
+  console.log(dataCharacters)
   return (
     <>
       <h1>Genshin App</h1>
@@ -12,6 +23,7 @@ function App() {
         })
       }
       </div>
+    
     </>
   );
 }
--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import CharacterModal from './CharacterModal';
+
 
 export default function CharacterCard({character}) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
+    <>
     <Card sx={{ maxWidth: 345 , width: 260 }}>
       <CardMedia
         sx={{ height: 260}}
@@ -17,12 +21,13 @@ export default function CharacterCard({character}) {
         <Typography gutterBottom variant="h5" component="div">
           {character.name}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-       {character.description}
-        </Typography>
+        
       </CardContent>
-      
+      <CharacterModal open={open} handleOpen={handleOpen} handleClose={handleClose} character={character}/>
     </Card>
     
+    </>
+
   );
+  
 }
